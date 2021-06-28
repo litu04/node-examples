3906bcf194b2ece8be46fcd7fc5adffe2fc5a45d
litu04 <debadattakar96@gmail.com>
2021-06-28 10:24:36 +0530

    new file rectangle added

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-var rect = {
-    area: (x,y) => x*y,
-    perimeter: (x,y) => (2*(x+y))
-};
+// var rect = {
+//     area: (x,y) => x*y,
+//     perimeter: (x,y) => (2*(x+y))
+// };
 
-function solveRect(l,b){
-    console.log("Solving for rectangle for l = "+ l + " and b = " + b);
+const rect = require('./rectangle');
 
-    if(l<=0 || b<=0){
+function solveRect(l, b) {
+    console.log("Solving for rectangle for l = " + l + " and b = " + b);
+
+    if (l <= 0 || b <= 0) {
         console.log(`Rectangle dimensions should be greater than Zero: l=${l} and b=${b}`);
     } else {
-        console.log("The area of the rectangle is " + rect.area(l,b));
-        console.log("The Perimeter of the rectangle is "+ rect.perimeter(l,b));
+        console.log("The area of the rectangle is " + rect.area(l, b));
+        console.log("The Perimeter of the rectangle is " + rect.perimeter(l, b));
     }
 }
 
-solveRect(2,3);
-solveRect(4,3);
-solveRect(0,5);
-solveRect(3,-3);
+solveRect(2, 5);
+solveRect(4, 6);
+solveRect(7, 0);
+solveRect(-3, 3);
---
+++ rectangle.js
@@ -0,0 +1,7 @@
+// const area = (x, y) => (x * y);
+// const perimeter = (x, y) => (2 * (x + y));
+
+// module.exports = {area,perimeter};
+
+exports.area = (x, y) => (x * y);
+exports.perimeter = (x, y) => (2 * (x + y));
